b3ffe40f47ae676e9c8b2d7ba89e0c9477302ff2
raine-works <mail@raineworks.com>
2024-06-21 01:27:09 +0000

    deploying

--- src/bun/install.sh
+++ src/bun/install.sh
@@ -28,5 +28,5 @@ export BUN_INSTALL=/usr/local
 if [ "${VERSION}" == "latest" ]; then
 	curl -fsSL https://bun.sh/install | bash
 else
-	curl -fsSL https://bun.sh/install | bash "bun-v${VERSION}"
+	curl -fsSL https://bun.sh/install | bash -s "bun-v${VERSION}"
 fi
